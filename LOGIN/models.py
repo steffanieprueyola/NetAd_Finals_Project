bb13b60d3e81efcefb00c010e88b62c6144010c1
Erica Mae L. Fernandez <fernandezericamaez@gmail.com>
2026-05-14 09:38:17 +0800

    Update models.py

--- LOGIN/models.py
+++ LOGIN/models.py
@@ -1,7 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-
-db = SQLAlchemy()
+from app import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
