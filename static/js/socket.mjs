c36c5454c19fa2e13874a2e26560affe276dde10
codexjcs <johnsasi39@gmail.com>
2026-05-14 11:27:09 +0800

    Update socket.mjs

--- static/js/socket.mjs
+++ static/js/socket.mjs
@@ -1,1 +1,19 @@
+import { io } from "https://cdn.socket.io/4.5.4/socket.io.esm.min.js";
 
+const socket = io();
+
+// listen for logs from backend
+socket.on("new_log", (data) => {
+
+    const logs = document.getElementById("logs");
+
+    const logEntry = document.createElement("div");
+    logEntry.classList.add("log-entry", data.type);
+
+    logEntry.innerHTML = `
+        <div>${data.message}</div>
+        <div class="log-time">${data.time}</div>
+    `;
+
+    logs.prepend(logEntry);
+});
