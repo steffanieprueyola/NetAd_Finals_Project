01a4125afde55a6867b25c858a8bfd31e104ba2c
codexjcs <johnsasi39@gmail.com>
2026-05-14 12:51:05 +0800

    Update routes.py

--- auth/routes.py
+++ auth/routes.py
@@ -1,27 +1,23 @@
 from flask import render_template, request, redirect, url_for, session, flash
 from auth import auth_bp
-from auth.models import users
+from auth.models import ADMIN
 
-# LOGIN PAGE
 @auth_bp.route("/")
 def login_page():
     return render_template("login.html")
 
-# LOGIN PROCESS
 @auth_bp.route("/login", methods=["POST"])
 def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    # check credentials
-    if username in users and users[username] == password:
-        session["user"] = username   # 🔐 create session
+    if username in ADMIN and ADMIN[username] == password:
+        session["user"] = username
         return redirect(url_for("dashboard.home"))
 
     flash("Invalid username or password")
     return redirect(url_for("auth.login_page"))
 
-# LOGOUT
 @auth_bp.route("/logout")
 def logout():
     session.clear()
